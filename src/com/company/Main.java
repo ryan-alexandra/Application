3ddd78fe137ad52450e7c926e8d7b8d09f47c90b
ryan.alexandra <ryan.alexandra@vashonsd.org>
2021-10-05 10:44:39 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(SquareOf(2));
+
+    }
+    public static int SquareOf(int n){
+        return n*n;
+    }
+}
